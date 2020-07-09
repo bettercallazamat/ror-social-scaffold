5536e6aef6ebf5c14bcc112cd37a8536c8482b84
bettercallazamat <bettercallazamat@gmail.com>
2020-07-09 21:15:14 +0600

    Add tests for models

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,11 @@
 class Friendship < ApplicationRecord
+  validate :not_self_friendship
+  validates :user_id, uniqueness: { scope: :friend_id }
+
+  def not_self_friendship
+    errors.add(:friend_id, 'User can not send a self friendship request.') if user_id == friend_id
+  end
+  
   belongs_to :user
   belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
 end
---
+++ spec/feature/friendship_spec.rb
@@ -0,0 +1,34 @@
+# require_relative '../rails_helper.rb'
+
+# RSpec.describe 'Friendship', type: :feature do
+#   before(:all) do
+#     visit new_user_registration_path
+#     fill_in 'user_name', with: 'johnsmith1'
+#     fill_in 'user_email', with: 'johnsmith1'
+#     fill_in 'user_password', with: 'passwordexample'
+#     fill_in 'user_password_confirmation', with: 'passwordexample'
+#     find("input[type='submit']").click
+#     click_on 'Sign out'
+#     visit new_user_registration_path
+#     fill_in 'user_name', with: 'johnsmith2'
+#     fill_in 'user_email', with: 'johnsmith2'
+#     fill_in 'user_password', with: 'passwordexample'
+#     fill_in 'user_password_confirmation', with: 'passwordexample'
+#     find("input[type='submit']").click
+#   end
+
+#   it 'should send Friendship requests' do
+#     visit users_path
+#     find_link('send-invitation-link').click
+#     expect(page).to have_content('Invitation sent')
+#   end
+
+#   it 'should accept Friendship requests' do
+#     visit new_user_session_path
+#     fill_in 'user_email', with: 'johnsmith1'
+#     fill_in 'user_password', with: 'passwordexample'
+#     visit users_path
+#     find_link('accept-invitation-link').click
+#     expect(page).to have_content('Friendship accepted!')
+#   end
+# end
